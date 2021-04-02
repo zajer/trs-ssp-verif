183dadbcf6dabf762c5f00d662c9d15e04d91b36
zajer <pikus3@list.pl>
2021-04-03 01:50:21 +0200

    Added unit tests for checking if update is possible at all.

--- lib/phase4.ml
+++ lib/phase4.ml
@@ -4,3 +4,18 @@ let is_update_with_trans_possible ~ui2state_map trans ~ui2par_map =
     let codom_mapping = Ui.make_map_of_list (Bigraph.Iso.to_list trans.TTS.participants) in
     let ui2state_map_from_par = Ui.transform_codom ~transformed_mapping:ui2par_map ~codom_mapping in
     Ui.is_subset ~subset:ui2state_map_from_par ~target:ui2state_map
+type mapped_trans = ((int*string),TTS.trans_exported) Hashtbl.t
+let rec _find_trans_allowing_for_update ~ui2state_map ~ui2par_map trans_with_same_key =
+    match trans_with_same_key with 
+    | [] -> false, None
+    | h::t -> 
+        if is_update_with_trans_possible ~ui2state_map ~ui2par_map h then
+            true,Some h
+        else
+            _find_trans_allowing_for_update ~ui2state_map ~ui2par_map t
+let is_update_possible ~ui2state_map ~ui2par_map trans_to_be_applied all_trans = 
+    let key = trans_to_be_applied.TTS.in_state_idx,trans_to_be_applied.react_label in
+    let known_similar_transitions = Hashtbl.find_all all_trans key in
+    _find_trans_allowing_for_update ~ui2state_map ~ui2par_map known_similar_transitions
+
+
--- test/phase4_tests.ml
+++ test/phase4_tests.ml
@@ -1,6 +1,6 @@
 open OUnit2
 open Ssp_verification
-let test_is_update_possible_1 _ =
+let test_is_update_with_trans_possible_1 _ =
   let ui2state_map = Ui.make_map_of_list [(7,0);(21,1);(777,2)]
   and ui2par_map = Ui.make_map_of_list [(777,0);(7,1)]
   and transition = 
@@ -20,7 +20,7 @@ let test_is_update_possible_1 _ =
     ~printer:(fun b -> string_of_bool b)
     expected_result
     result
-let test_is_update_possible_2 _ =
+let test_is_update_with_trans_possible_2 _ =
   let ui2state_map = Ui.make_map_of_list [(7,0);(21,1);(777,2)]
   and ui2par_map = Ui.make_map_of_list [(777,1);(7,0)]
   and transition = 
@@ -40,7 +40,7 @@ let test_is_update_possible_2 _ =
     ~printer:(fun b -> string_of_bool b)
     expected_result
     result
-let test_is_update_possible_3 _ =
+let test_is_update_with_trans_possible_3 _ =
   let ui2state_map = Ui.make_map_of_list [(7,0);(21,1);(777,2)]
   and ui2par_map = Ui.make_map_of_list [(5,0);(7,1)]
   and transition = 
@@ -60,11 +60,140 @@ let test_is_update_possible_3 _ =
     ~printer:(fun b -> string_of_bool b)
     expected_result
     result
+let test_is_update_possible_1 _ =
+  let ui2state_map = Ui.make_map_of_list [(7,0);(21,1);(777,2)]
+  and ui2par_map = Ui.make_map_of_list [(777,0);(7,1)] 
+  and applied_transition = 
+    {
+      Tracking_bigraph.TTS.in_state_idx=(1);
+      out_state_idx=(-1);
+      react_label="my react";
+      participants=(Bigraph.Iso.of_list [(0,1);(1,2)]);
+      residue=Bigraph.Fun.empty;
+      actual_out_state=Bigraph.Big.one
+    }
+  and known_transition_1 = 
+    {
+      Tracking_bigraph.TTS.in_state_idx=(1);
+      out_state_idx=(-1);
+      react_label="my react";
+      participants=(Bigraph.Iso.of_list [(0,1);(1,2)]);
+      residue=Bigraph.Fun.empty;
+      actual_out_state=Bigraph.Big.one
+    }
+  and known_transition_2 =
+    {
+      Tracking_bigraph.TTS.in_state_idx=(2);
+      out_state_idx=(-1);
+      react_label="my react";
+      participants=(Bigraph.Iso.of_list [(0,2);(1,0)]);
+      residue=Bigraph.Fun.empty;
+      actual_out_state=Bigraph.Big.one
+    }
+  and known_transition_3 =
+    {
+      Tracking_bigraph.TTS.in_state_idx=(1);
+      out_state_idx=(-1);
+      react_label="my react2";
+      participants=(Bigraph.Iso.of_list [(0,2);(1,0)]);
+      residue=Bigraph.Fun.empty;
+      actual_out_state=Bigraph.Big.one
+    }
+  and known_transition_4 = 
+    {
+      Tracking_bigraph.TTS.in_state_idx=(1);
+      out_state_idx=(-1);
+      react_label="my react";
+      participants=(Bigraph.Iso.of_list [(0,2);(1,0)]);
+      residue=Bigraph.Fun.empty;
+      actual_out_state=Bigraph.Big.one
+    }
+    in
+  let known_transitions = [known_transition_1;known_transition_2;known_transition_3;known_transition_4] in
+  let known_trans_dict = 
+    Seq.unfold 
+      (
+        fun list -> 
+          match list with 
+          | [] -> None
+          | h::t -> 
+            let dict_elem_key = h.Tracking_bigraph.TTS.in_state_idx,h.react_label
+            and dict_elem_val = h in
+            Some ((dict_elem_key,dict_elem_val),t)
+      ) 
+      known_transitions |> Hashtbl.of_seq in
+  let result = Phase4.is_update_possible ~ui2state_map ~ui2par_map applied_transition known_trans_dict
+  and expected_result = true, Some known_transition_4 in
+  assert_equal
+    ~msg:"Result of is_update_possible is not equal to expected"
+    expected_result
+    result
+let test_is_update_possible_2 _ =
+  let ui2state_map = Ui.make_map_of_list [(7,0);(21,1);(777,2)]
+  and ui2par_map = Ui.make_map_of_list [(777,0);(7,1)] 
+  and applied_transition = 
+    {
+      Tracking_bigraph.TTS.in_state_idx=(1);
+      out_state_idx=(-1);
+      react_label="my react";
+      participants=(Bigraph.Iso.of_list [(0,1);(1,2)]);
+      residue=Bigraph.Fun.empty;
+      actual_out_state=Bigraph.Big.one
+    }
+  and known_transition_1 = 
+    {
+      Tracking_bigraph.TTS.in_state_idx=(1);
+      out_state_idx=(-1);
+      react_label="my react";
+      participants=(Bigraph.Iso.of_list [(0,1);(1,2)]);
+      residue=Bigraph.Fun.empty;
+      actual_out_state=Bigraph.Big.one
+    }
+  and known_transition_2 =
+    {
+      Tracking_bigraph.TTS.in_state_idx=(2);
+      out_state_idx=(-1);
+      react_label="my react";
+      participants=(Bigraph.Iso.of_list [(0,2);(1,0)]);
+      residue=Bigraph.Fun.empty;
+      actual_out_state=Bigraph.Big.one
+    }
+  and known_transition_3 =
+    {
+      Tracking_bigraph.TTS.in_state_idx=(1);
+      out_state_idx=(-1);
+      react_label="my react2";
+      participants=(Bigraph.Iso.of_list [(0,2);(1,0)]);
+      residue=Bigraph.Fun.empty;
+      actual_out_state=Bigraph.Big.one
+    }
+    in
+  let known_transitions = [known_transition_1;known_transition_2;known_transition_3] in
+  let known_trans_dict = 
+    Seq.unfold 
+      (
+        fun list -> 
+          match list with 
+          | [] -> None
+          | h::t -> 
+            let dict_elem_key = h.Tracking_bigraph.TTS.in_state_idx,h.react_label
+            and dict_elem_val = h in
+            Some ((dict_elem_key,dict_elem_val),t)
+      ) 
+      known_transitions |> Hashtbl.of_seq in
+  let result = Phase4.is_update_possible ~ui2state_map ~ui2par_map applied_transition known_trans_dict
+  and expected_result = false, None in
+  assert_equal
+    ~msg:"Result of is_update_possible is not equal to expected"
+    expected_result
+    result
 let suite =
     "Phase 4" >::: [
+        "Is update with trans possible test 1">:: test_is_update_with_trans_possible_1;
+        "Is update with trans possible test 2">:: test_is_update_with_trans_possible_2;
+        "Is update with trans possible test 3">:: test_is_update_with_trans_possible_3;
         "Is update possible test 1">:: test_is_update_possible_1;
         "Is update possible test 2">:: test_is_update_possible_2;
-        "Is update possible test 3">:: test_is_update_possible_3;
     ]
 
 let () =
