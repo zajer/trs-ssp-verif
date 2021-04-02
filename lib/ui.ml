3b02ca3e1f9cd43ab60d8abe4f1d284fc8b5bcde
zajer <pikus3@list.pl>
2021-04-02 06:50:38 +0200

    Added another test of possibility of update. Fixes to make the above test pass.

--- lib/ui.ml
+++ lib/ui.ml
@@ -1,9 +1,9 @@
 module IntMapping =
 struct
     type t = int * int
-    let compare (ui0,_) (ui1,_) =
+    let compare (ui0,v0) (ui1,v1) =
     match Stdlib.compare ui0 ui1 with
-        | 0 -> 0
+        | 0 -> Stdlib.compare v0 v1
         | c -> c
 end
 module IntMappingsSet = Set.Make(IntMapping)
@@ -15,7 +15,7 @@ let transform_codom ~transformed_mapping ~codom_mapping =
     IntMappingsSet.map 
         (
             fun (ui,mapped_val) -> 
-                let corresponding_transformation = IntMappingsSet.find (mapped_val,-1) codom_mapping in
+                let corresponding_transformation = IntMappingsSet.find_first (fun (chk_ui,_) -> chk_ui = mapped_val) codom_mapping in
                 match corresponding_transformation with
                 | _, new_mapped_val -> ui,new_mapped_val
         )
--- test/phase4_tests.ml
+++ test/phase4_tests.ml
@@ -20,10 +20,31 @@ let test_is_update_possible_1 _ =
     ~printer:(fun b -> string_of_bool b)
     expected_result
     result
+let test_is_update_possible_2 _ =
+  let ui2state_map = Ui.make_map_of_list [(7,0);(21,1);(777,2)]
+  and ui2par_map = Ui.make_map_of_list [(777,1);(7,0)]
+  and transition = 
+    {
+        Tracking_bigraph.TTS.in_state_idx=(-1);
+        out_state_idx=(-1);
+        react_label="my react";
+        participants=(Bigraph.Iso.of_list [(0,2);(1,0)]);
+        residue=Bigraph.Fun.empty;
+        actual_out_state=Bigraph.Big.one
+    }
+  in
+  let result = Phase4.is_update_possible ~ui2state_map transition ~ui2par_map
+  and expected_result = false in
+  assert_equal
+    ~msg:"Result not equal to expected"
+    ~printer:(fun b -> string_of_bool b)
+    expected_result
+    result
   
 let suite =
     "Phase 4" >::: [
-        "Is update possible test 1">:: test_is_update_possible_1
+        "Is update possible test 1">:: test_is_update_possible_1;
+        "Is update possible test 2">:: test_is_update_possible_2
     ]
 
 let () =
