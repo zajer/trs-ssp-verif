4a4946d6df234969cd0101b3a8bb14772d59af32
zajer <pikus3@list.pl>
2021-04-03 04:13:34 +0200

    First version of constructed state update function. Needs testing.

--- lib/phase4.ml
+++ lib/phase4.ml
@@ -24,4 +24,24 @@ let _is_function_injective f =
         true
     else
         false
-        
+let _update_mapping_with_new_objs ui_to_update residue output_state new_ui = 
+    let dom_of_residue = Bigraph.Fun.dom residue in
+    let nodes_unmapped_by_residue,num_of_unmapped_nodes = 
+        Bigraph.Nodes.fold 
+        (fun i _ (unmapped_nodes,num_of_unmapped_nodes) -> 
+            if Bigraph.IntSet.mem i dom_of_residue then (i::unmapped_nodes),num_of_unmapped_nodes+1 else unmapped_nodes,num_of_unmapped_nodes 
+        ) 
+        output_state 
+        ([],0) in
+    let result = List.mapi (fun i unmapped_node_id -> new_ui+i, unmapped_node_id) nodes_unmapped_by_residue in
+    let mapping_of_new_elems = Ui.make_map_of_list result in
+    Ui.IntMappingsSet.union ui_to_update mapping_of_new_elems,num_of_unmapped_nodes+new_ui
+let update ui2state_map trans all_states first_new_ui = 
+    match _is_function_injective trans.TTS.residue with
+    | false -> raise (Invalid_argument "Residue is not an injective function")
+    | true -> 
+        let residue_inverted = Bigraph.Fun.to_list trans.residue |> List.map (fun (i1,i2)-> i2,i1) |> Ui.make_map_of_list in
+        let ui2state_map_updated_by_residue = Ui.transform_codom ~transformed_mapping:ui2state_map ~codom_mapping:residue_inverted 
+        and result_state = {TTS.bigraph=(Hashtbl.find all_states trans.out_state_idx);index=trans.out_state_idx} in
+        let result_ui2state_map,new_first_ui_val = _update_mapping_with_new_objs ui2state_map_updated_by_residue trans.residue result_state.bigraph.n first_new_ui in
+        (Hashtbl.find all_states trans.out_state_idx),result_ui2state_map,new_first_ui_val
