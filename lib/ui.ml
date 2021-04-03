cc9e4cd384f71ad11682704d2ad516d9c2b02c16
zajer <pikus3@list.pl>
2021-04-03 21:36:22 +0200

    Modified how UI module works. Its elements are now compared only by domain (codomain is omitted in comparison).

--- lib/ui.ml
+++ lib/ui.ml
@@ -1,9 +1,9 @@
 module IntMapping =
 struct
     type t = int * int
-    let compare (ui0,v0) (ui1,v1) =
+    let compare (ui0,_) (ui1,_) =
     match Stdlib.compare ui0 ui1 with
-        | 0 -> Stdlib.compare v0 v1
+        | 0 -> 0
         | c -> c
 end
 module IntMappingsSet = Set.Make(IntMapping)
@@ -15,7 +15,7 @@ let transform_codom ~transformed_mapping ~codom_mapping =
     IntMappingsSet.map 
         (
             fun (ui,mapped_val) -> 
-                let corresponding_transformation = IntMappingsSet.find_first_opt (fun (chk_ui,_) -> chk_ui = mapped_val) codom_mapping in
+                let corresponding_transformation = IntMappingsSet.find_opt (mapped_val,-1) codom_mapping in
                 match corresponding_transformation with
                 | Some (_, new_mapped_val) -> ui,new_mapped_val
                 | None -> ui,mapped_val
@@ -34,5 +34,9 @@ let union ~base ~extension =
     )
     extension
     base
+let are_equal s1 s2 = 
+    let s1els = IntMappingsSet.elements s1
+    and s2els = IntMappingsSet.elements s2 in
+    List.for_all2 (fun es1 es2 -> es1 = es2) s1els s2els
 let mapping_to_string (i1,i2) = "("^(string_of_int i1 )^","^(string_of_int i2)^")"
 let map_to_string map = let res = IntMappingsSet.elements map |> List.map (fun mapping -> mapping_to_string mapping) |> String.concat ";" in "{"^res^"}"
