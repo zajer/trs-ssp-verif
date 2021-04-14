6e296a93e84ae2c370f25328b972613ae6f1e12e
zajer <pikus3@list.pl>
2021-04-14 06:56:50 +0200

    Renamed types in Phase3 and Phase4 modules. Deleted unused type in Phase4 module.

--- lib/phase4.ml
+++ lib/phase4.ml
@@ -1,10 +1,9 @@
 open Tracking_bigraph
-type mapped_reacts = (int,Tracking_bigraph.TTS.trans_exported) Hashtbl.t
 let is_update_with_trans_possible ~ui2state_map trans ~ui2par_map =
     let codom_mapping = Ui.make_map_of_list (Bigraph.Iso.to_list trans.TTS.participants) in
     let ui2state_map_from_par = Ui.transform_codom false ~transformed_mapping:ui2par_map ~codom_mapping in
     Ui.is_subset ~subset:ui2state_map_from_par ~target:ui2state_map
-type mapped_trans = ((int*string),TTS.trans_exported) Hashtbl.t
+type mapped_trans_by_key = ((int*string),TTS.trans_exported) Hashtbl.t
 let rec _find_trans_allowing_for_update ~ui2state_map ~ui2par_map trans_with_same_key =
     match trans_with_same_key with 
     | [] -> false, None
