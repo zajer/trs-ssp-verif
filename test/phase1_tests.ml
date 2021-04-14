4b41e99b924155842b2a659afdad910b6612282f
zajer <pikus3@list.pl>
2021-04-14 03:07:13 +0200

    Added another test for the main routine of phase 1.

--- test/phase1_tests.ml
+++ test/phase1_tests.ml
@@ -35,7 +35,7 @@ let _compare_constructed_states sl1 sl2 =
     else
         false
 let _constructed_state2string cs = 
-    "(state:"^(cs.Phase3.state.index |> string_of_int)^" ui map:"^(Ui.map_to_string cs.ui_map)^" time:"^(string_of_int cs.time)^")"
+    "(state:"^(cs.Phase3.state.index |> string_of_int)^" ui map:"^(Ui.map_to_string cs.ui_map)^" time:"^(string_of_int cs.time)^" SAT config:"^(Ssp.Template_state.to_stirng cs.sat_config)^")"
 let test_perform_phase_1 _ = 
     let states_filename = "exmp_1_states.csv"
     and trans_filename = "exmp_1_trans_normed.csv"
@@ -94,10 +94,63 @@ let test_perform_phase_1 _ =
         ~printer:(fun sl -> let result = List.map _constructed_state2string sl |> String.concat "\n\t" in "\n[\n\t"^result^"\n]" )
         expected_states
         result_constructed_states
-    
+let test_perform_phase_2 _ = 
+    let states_filename = "exmp_2_states.csv"
+    and trans_filename = "exmp_2_trans_normed.csv"
+    and walk_filename = "exmp_2_walk.csv"
+    and number_of_agents = 2 in
+    let states = Tracking_bigraph.TTS.import_states states_filename
+    and trans = Tracking_bigraph.TTS.import_transitions ~headers_in_first_row:false trans_filename
+    and walk = Ssp.Frontend.import_trans_funs walk_filename in
+    let states_by_idx = _map_states_to_idx states
+    and trans_by_idx = _map_trans_to_idx trans
+    and trans_by_key = _map_trans_to_key trans in
+    let result = Phase1.perform_phase walk ~num_of_agents:number_of_agents states_by_idx trans_by_idx trans_by_key (fun _ x -> x)
+    and expected_status = true 
+    and expected_states =
+        [
+            {
+                Phase3.state=(List.nth states 3);
+                ui_map=[(1,0);(2,1);(3,2);(4,3);(5,4);(6,5)] |> Ui.make_map_of_list;
+                sat_config=[|(1,0);(2,0)|];time=0
+            };
+            {
+                Phase3.state=(List.nth states 1);
+                ui_map=[(1,0);(2,2);(3,3);(4,1);(5,4);(6,5)] |> Ui.make_map_of_list;
+                sat_config=[|(1,1);(2,0)|];time=1
+            };
+            {
+                Phase3.state=(List.nth states 3);
+                ui_map=[(1,0);(2,1);(3,2);(4,3);(5,4);(6,5)] |> Ui.make_map_of_list;
+                sat_config=[|(1,2);(2,0)|];time=2
+            };
+            {
+                Phase3.state=(List.nth states 5);
+                ui_map=[(5,0);(4,1);(3,2);(2,3);(1,4);(6,5)] |> Ui.make_map_of_list;
+                sat_config=[|(1,3);(2,3)|];time=3
+            };
+            {
+                Phase3.state=(List.nth states 2);
+                ui_map=[(4,0);(6,1);(2,2);(3,3);(1,4);(5,5)] |> Ui.make_map_of_list;
+                sat_config=[|(1,4);(2,4)|];time=4
+            }
+        ]
+     in
+    assert_equal
+        ~msg:"Phase 1 should end successfuly"
+        expected_status
+        result.is_successful;
+    let result_constructed_states,_ = result.value in
+    assert_equal
+        ~msg:"Constructed states are not equal to expected"
+        ~cmp:_compare_constructed_states
+        ~printer:(fun sl -> let result = List.map _constructed_state2string sl |> String.concat "\n\t" in "\n[\n\t"^result^"\n]" )
+        expected_states
+        result_constructed_states   
 let suite =
     "Phase 1" >::: [
-        "Perform phase test">:: test_perform_phase_1;
+        "Perform phase test 1">:: test_perform_phase_1;
+        "Perform phase test 2">:: test_perform_phase_2;
     ]
 
 let () =
