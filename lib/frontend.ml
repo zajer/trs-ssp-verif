fd07a5f11f3b4cce34fdac789e0e7371b3dbe64b
zajer <pikus3@list.pl>
2021-04-22 07:26:03 +0200

    Extended Frontend module.

--- lib/frontend.ml
+++ lib/frontend.ml
@@ -18,4 +18,19 @@ let map_trans_to_key trans_list =
         )
         trans_list;
     result
-
+type config_raw = {directory:string; special_name:string;controls_2_shape:(string*string) list; controls_2_color:(string*string) list} [@@deriving of_yojson]
+type config = {directory:string; special_name:string;control2shape:(string,string)Hashtbl.t; control2color:(string,string) Hashtbl.t}
+let convert config_raw =
+    let control2shape = Hashtbl.create (List.length config_raw.controls_2_shape)
+    and control2color = Hashtbl.create (List.length config_raw.controls_2_color) in
+    List.iter (fun (control,color) -> Hashtbl.add control2color control color ) config_raw.controls_2_color;
+    List.iter (fun (control,shape) -> Hashtbl.add control2shape control shape ) config_raw.controls_2_shape;
+        {directory=config_raw.directory; special_name=config_raw.special_name;control2shape; control2color}
+let make_visjs_timeline_config config = 
+    {Visjs.directory=config.directory;name=config.special_name;known_objects=Common.IntSet.empty;current_timeline=[];}
+let make_visjs_serialization_config config = 
+    {Visjs.directory=config.directory;file_prefix=config.special_name; control2shape=config.control2shape; control2color=config.control2color}
+let make_output_info config =
+    let tmp1 = make_visjs_timeline_config config
+    and tmp2 = make_visjs_serialization_config config in
+    {Phase1.is_scenario_valid=true;message=None;directory=config.directory;timeline_filename=Visjs.timeline_filename tmp1; groups_filename=Visjs.groups_filename tmp1; states_regex=Visjs.state_serialized_filename_regex tmp2}
